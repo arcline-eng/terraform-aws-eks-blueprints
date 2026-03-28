afd974dfebbaab5198e9b9804535480589ea5604
srikanthsomar <97084610+srikanthsomar@users.noreply.github.com>
2026-03-28 19:09:45 +0530

    Updating r6.16xlarge to r6g.16xlarge as the former doesn't exist

--- docs/v4-to-v5/example/v5.tf
+++ docs/v4-to-v5/example/v5.tf
@@ -83,7 +83,7 @@ module "eks" {
       launch_template_name            = "self_managed-${local.name}" # Backwards compat
       launch_template_use_name_prefix = false                        # Backwards compat
 
-      instance_type = "r6.16xlarge"
+      instance_type = "r6g.16xlarge"
 
       min_size     = 1
       max_size     = 2
