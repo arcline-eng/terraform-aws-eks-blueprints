997be2b84bd4e6a642a19fad78ce0b8671a84d2c
Bhargava Sai Deepak Mathukumalli <132651360+mbsdeepak-amnic@users.noreply.github.com>
2026-06-19 15:18:51 +0530

    Update instance types for Bottlerocket node group

--- patterns/bottlerocket/eks.tf
+++ patterns/bottlerocket/eks.tf
@@ -34,7 +34,7 @@ module "eks" {
   eks_managed_node_groups = {
     bottlerocket = {
       ami_type       = "BOTTLEROCKET_x86_64"
-      instance_types = ["m5.large", "m5a.large"]
+      instance_types = ["m5.xlarge", "m5a.xlarge"]
 
       iam_role_attach_cni_policy = true
       # The below AMI release version is for testing purposes in order to validate Botterocket Update Operator.
